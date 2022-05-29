b7f71e14f7c1e61b21e0f342b58e2b1457658237
Mame Coumba Ka <mck@dhcp-10-105-91-6.wireless.northwestern.private>
2022-05-28 19:59:37 -0500

    vazy

--- static/js/bookmatch.js
+++ static/js/bookmatch.js
@@ -1,29 +1,43 @@
 
+const displaysearch = () => {
+    const html = 
+    `<input type="text" name="search" placeholder="Search for a book">
+    <button onclick = "displayresults(event);"> Search </button>`;
 
+    document.querySelector('#search-bar').innerHTML = html;
+};
+const book2html = book => {
+    return
+     `
+    <div class = "book-card">
+    <img src="https://covers.openlibrary.org/b/ID/${book['ID']}-L.jpg">
+    <div class = "title" >${book['Title']}</div>
+    <div class = "author">${book['Author']}</div>
+    <button class = "add-button">Add to Library</button>
+</div>`
+}
+const makeBook = book => {
+    return 
+}
 
-const displayresults = () => {
+const displayresults = ev => {
 
-    var search = document.querySelector('input').value;
-    console.log(search);
-    fetch('/api/results?text=${search}')
+    const elem = ev.currentTarget;
+    const search = elem.parentElement.querySelector('input').value;
+
+    
+    fetch('/api/results?search= ' + search)
 
     
         .then(response => response.json())
-        .then(data => {
-            const 
-            document.querySelector('#results').innerHTML = 
+        .then (results => {
+            console.log(results);
+            const html = results.slice(0,10).map(result2html).join('');
+            document.querySelector('#search-results').innerHTML = html;
+        })
+        
+    
+};
 
         
-
-        
-
-
-        
-
-}
-
-        
-
-
-
-        
+displaysearch();
--- views/books.py
+++ views/books.py
@@ -4,7 +4,7 @@ from libgen_api import LibgenSearch
 # from flask import Blueprint, render_template, request, redirect, url_for, flash
 from flask import Response, request
 from flask_restful import Resource
-
+from models import Book, db
 import json
 
 def get_path():
@@ -15,9 +15,11 @@ class BookListEndpoint(Resource):
 
     def get (self) : 
         args = request.args
+
         s = LibgenSearch()
-        text = args.get('text')
+        text = args.get('search')
         results = s.search_title(text)
+
         
         
         return Response(json.dumps(results), mimetype="application/json", status=200)
